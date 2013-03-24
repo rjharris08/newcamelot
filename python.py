d1a324424b8347c1f854c7823de5826b50487c72
Ron Harris <mr_rjharris@yahoo.com>
2013-03-24 08:37:12 -0700

    new file: python.py

---
+++ python.py
@@ -0,0 +1,7 @@
+#!C:/python27/python
+
+print "Content-Type: text/html"     # HTML is following
+print                               # blank line, end of headers
+print "<TITLE>CGI script output</TITLE>"
+print "<H1>This is my first CGI script</H1>"
+print "Hello, world!"
